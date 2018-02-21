f446b86fb2599c75a0ef73fb4c4bae65ae4a00db
EthanSausville <ethan.sausville@gmail.com>
2018-02-21 13:07:42 -0800

    Upload

---
+++ ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Wolverine fewest battles
+            // Pheonix most battles
+
+            int j = 0;
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string mostBattlesName = "";
+            int mostBattlesAmount = 0;
+
+            for (int i = 0; i < names.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[j])
+                {
+                    j++;
+                    mostBattlesName = names[j];
+                    mostBattlesAmount = numbers[j];
+                }
+            }
+
+            string leastBattlesName = "";
+            int leastBattlesAmount = 0;
+
+            for (int i = 0; i < names.Length - 1; i++)
+            {
+                if (numbers[i] < numbers[j])
+                {
+                    j++;
+                    leastBattlesName = names[j];
+                    leastBattlesAmount = numbers[j];
+                }
+            }
+
+
+            resultLabel.Text = string.Format("{0} has fought the most battles. {0} fought {1} battles." +
+                "<br />{2} has fought the least battles. {2} fought {3} battles.",
+                mostBattlesName, mostBattlesAmount, leastBattlesName, leastBattlesAmount);
+        }
+    }
+}
